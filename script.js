a16d87e1f8125236b50746742c2be9e47dcc2fc6
Jasurbek Isokov <jasurisoqov685@gmail.com>
2022-04-05 07:02:21 +0300

    html css sections fininshed

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 "use script";
 
 //  https://api.github.com/users/defunkt -> bu yerda users dan keyin username keladi
+
+const search = document.querySelector(".search");
+const profileBtn = document.querySelecto(".profile_btn");
+const publicRepos = document.querySelector(".public_repos-text");
+const publikGists = document.querySelector(".publik_gists-text");
+const followers = document.querySelector(".followers-text");
+const following = document.querySelector(".following");
+const company = document.querySelector(".company_paragraph");
+const blog = document.querySelector("blog_paragraph");
+const location = document.querySelector(".location_paragraph");
+const member = document.querySelector(".member_paragraph");
